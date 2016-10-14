218613134463b943351eb909db6023717215c3e9
Yvonne Chan <ychan1352@gmail.com>
2016-10-14 12:33:34 -0400

    started stuff

--- db_builder.py
+++ db_builder.py
@@ -8,15 +8,31 @@ db = sqlite3.connect(f) #open if f exists, otherwise create
 c = db.cursor()    #facilitate db ops
 
 #==========================================================
-#INSERT YOUR POPULATE CODE IN THIS ZONE
-#...perhaps by beginning with these examples...
 
-'''
+fObj = open("peeps.csv") 
+d=csv.DictReader(fObj)
+
+
+    #Q: What can you print here to make each line show only
+    #   a name and its ID?
+    #   eg,
+    #   sasha, 3
+
 q = "CREATE TABLE students (name TEXT, id INTEGER)"
+
+for key in d:
+    ans = ""
+    for name in key:
+        if name == 'name':
+            field1 = key[name]
+        if name == 'id':
+            field2 = key[name]
+    q+= "INSERT INTO students VALUES(" + field1 +","+field2+");"
+
 
 c.execute(q)    #run SQL query
 
-
+'''
 q = "CREATE TABLE courses (code TEXT, id INTEGER, mark INTEGER)"
 
 c.execute(q)
