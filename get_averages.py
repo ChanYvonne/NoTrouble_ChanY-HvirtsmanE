689aea34e898935ab9f366bf7cbcb5ad73c8417b
Yvonne Chan <ychan1352@gmail.com>
2016-10-19 13:47:35 -0400

    something

--- get_averages.py
+++ get_averages.py
@@ -1,21 +1,18 @@
 import sqlite3   #enable control of an sqlite database
 import csv       #facilitates CSV I/O
 
-q = "CREATE TABLE students (name TEXT, id INTEGER, average INTEGER)"
+f = "discobandit.db"
 
-c.execute(q)    #run SQL query
-
-for key in d1:
-    field1 = key['name']
-    field2 = key['id']
-    field3 = key['average']
-    q = "INSERT INTO students VALUES('" + field1 + "','" +field2 + "','"+ field3+ "');"
-    c.execute(q)
-
-db = sqlite3.connect(“discobandit.db”)
+db = sqlite3.connect(f)
 cur = db.cursor()
-cmd = “SELECT mark FROM students,courses WHERE students.id = courses.id”
+cmd = "SELECT name, mark FROM students,courses WHERE students.id = courses.id"
 cur.execute(cmd)
 sel = cur.execute(cmd)
+
+int sum = 0
+int amt = 0
 for record in sel:
-	print record
+    if students.id == courses.id:
+        sum += record['mark']
+        amt += 1
+    
